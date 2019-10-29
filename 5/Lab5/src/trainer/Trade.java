5b377b7c4e6e610140c058fde7427b9f93433eb1
Joe-a-d <2334590D@student.gla.ac.uk>
2019-10-29 16:36:26 +0000

    Add trade class and methods. Final

--- 5/Lab5/src/trainer/Trade.java
+++ 5/Lab5/src/trainer/Trade.java
@@ -1,5 +1,50 @@
 package trainer;
 
+import monster.* ;
+
 public class Trade {
+	
+	private Trainer trainer1;
+	private Trainer trainer2;
+	private Monster monster1;
+	private Monster monster2;
+	
+	/**
+	 * 
+	 * Checks if monster is in each trainer's {@link Trainer#monsterSet}; if it is not 
+	 *	an exception is thrown, otherwise a new Trainer object is created
+	 * @param trainer1
+	 * @param monster1
+	 * @param trainer2
+	 * @param monster2
+	 * @throws MonsterException
+	 */
+	public Trade(Trainer trainer1 , Monster monster1 , Trainer trainer2, Monster monster2)
+		throws MonsterException{
+		
+		if(!trainer1.hasMonster(monster1) || !trainer2.hasMonster(monster2)) {
+			throw new MonsterException("At least one of the trainers does not have that monster in hers set");
+		}else {
+			this.trainer1 = trainer1;
+			this.monster1 = monster1;
+			this.trainer2 = trainer2;
+			this.monster2 = monster2;
+		}
+		
+	}
+	
+	/**
+	 * Performs trade by calling the add and remove methods from the {@link Trainer} Class
+	 */
+	
+	public void doTrade() {
+		trainer1.addMonster(monster2);
+		trainer1.removeMonster(monster1);
+		trainer2.addMonster(monster1);
+		trainer2.removeMonster(monster2);
+		
+	}
 
 }
+
+	
--- 5/Lab5/src/trainer/Trainer.java
+++ 5/Lab5/src/trainer/Trainer.java
@@ -1,83 +1,85 @@
 package trainer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Assert;
+import monster.*;
 
-import monster.Attack;
-import monster.FireMonster;
-import monster.Monster;
-import monster.WaterMonster;
-
-
-/*
- * TUTOR:
- * 	-> Are we assuming that each monster traits are unique? 
- * 				If not, how are names linked to Objects, mapping possible, but test doesn't pass string,
- * 					just the variable name containing the Monster instance
- */
 
 public class Trainer {
-	
-	private String name;
-	private ArrayList<Monster> monsterSet = new ArrayList<Monster> ();
-	private Map<String,Integer> monsterMap = new HashMap<>();
 
-	
-	
+/* Data Structures
+ * 	monsterSet contains an array of Monster objects for the current Trainer
+ *  monsterMap creates a map (k,v) where k = Monster type and v = Type count 
+ */
+	private String name;
+	private ArrayList<Monster> monsterSet = new ArrayList<Monster>();
+	private Map<String, Integer> monsterMap = new HashMap<>();
 
 	public Trainer(String name) {
 		this.name = name;
-		
-	}
 
+	}
 
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	/**
+	 * Adds the monster passed as argument to the trainer's set if monster is not already in {@link Trainer#monsterSet}
+	 * @param monster
+	 * @return Boolean
+	 */
 	public boolean addMonster(Monster monster) {
-		if (this.hasMonster(monster)){
+		if (this.hasMonster(monster)) {
 			return false;
-		}else {
+		} else {
 			this.monsterSet.add(monster);
 			return true;
 		}
 	}
 
-
+	/**
+	 * 
+	 * Checks if a given monster is in {@link Trainer#monsterSet}
+	 * @param monster
+	 * @return Boolean
+	 */
 	public boolean hasMonster(Monster monster) {
-		if(this.monsterSet.contains(monster)) {
-			return true;
-		}else {
-			return false;
-		}
+		return this.monsterSet.contains(monster);
 	}
 
-
+	/**
+	 * Removes the monster passed as an argument from the trainer's set if monster is not already in {@link Trainer#monsterSet}.
+	 * @param monster
+	 * @return Boolean
+	 */
 	public boolean removeMonster(Monster monster) {
-		if (!hasMonster(monster)) {
-			return true;
-		}else {
-			this.monsterSet.remove(monster);
+		if (!this.hasMonster(monster)) {
 			return false;
+		} else {
+			this.monsterSet.remove(monster);
+			return true;
 		}
-		
+
 	}
 
-	public Map<String,Integer> countMonstersByType() {
-		int countFire=0; int countElectric = 0; int countWater = 0;
-		for(Monster monster : this.monsterSet) {
-			switch(monster.getType()) {
-			
+	/**
+	 * Creates {@link Trainer#monsterMap}
+	 * @return Boolean 
+	 */
+	public Map<String, Integer> countMonstersByType() {
+		int countFire = 0;
+		int countElectric = 0;
+		int countWater = 0;
+		for (Monster monster : this.monsterSet) {
+			switch (monster.getType()) {
+
 			case "Fire":
 				countFire++;
 				break;
@@ -87,68 +89,22 @@ public class Trainer {
 			case "Water":
 				countWater++;
 				break;
-				
+
 			}
 		}
-			
-			
-				this.monsterMap.put("Fire", countFire);
-				this.monsterMap.put("Electric", countElectric);
-				this.monsterMap.put("Water", countWater);
-			
-			
-		
+
+		this.monsterMap.put("Fire", countFire);
+		this.monsterMap.put("Electric", countElectric);
+		this.monsterMap.put("Water", countWater);
+
 		return this.monsterMap;
 	}
 
-	
-
-
-	
-
 	@Override
 	public String toString() {
-		return "*****Trainer****" + "\n" + " Name = " + name + "\n" + " Monsters = "  + monsterSet  + "\n" + " #Monsters = " + monsterMap + "\n" + "****************" ;
+		return "*****Trainer****" + "\n" + " Name = " + name + "\n" + " Monsters = " + monsterSet + "\n"
+				+ " #Monsters = " + monsterMap + "\n" + "****************";
 	}
 
-
-	public static void main(String[] args) {
-		FireMonster charizard = new FireMonster (130, new Attack[] {
-				new Attack("Fire Spin", 14), new Attack("Blast Burn", 110),
-				new Attack("Overheat", 160)});
-		WaterMonster asda = new WaterMonster (120, new Attack[] {
-				new Attack("Fire Spin", 14), new Attack("Blast Burn", 110),
-				new Attack("Overheat", 160)});
-
-		Trainer ethan = new Trainer("ethan");
-		ArrayList<Monster> ms = ethan.monsterSet;
-
-		System.out.println(ethan.addMonster(charizard));
-		System.out.println(ethan.addMonster(asda));
-		//System.out.println(ms);
-		//System.out.println(ethan.removeMonster(charizard));
-		//System.out.println(ms);
-		
-		//System.out.println("monster".equalsIgnoreCase(Monster.class.getPackageName()));
-		System.out.println(ethan.monsterSet);
-		System.out.println(ethan.countMonstersByType());
-		
-		Map<String, Integer> expected = new HashMap<>();
-		expected.put("Water", 1);
-		expected.put("Fire", 1);
-		expected.put("Electric",0);
-
-		System.out.println(expected);
-		System.out.println(expected.equals(ethan.countMonstersByType()));
-		
-		System.out.println(ethan.toString());
-		
-		
-		
-	
-	
-	}
-	
-	
 	
 }
